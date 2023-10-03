f4897f312abb11221611a3f31dc63bf1ecef91fa
trantuong676 <128342689+trantuong676@users.noreply.github.com>
2023-10-03 11:14:33 +0700

    the firts

---
+++ BTL06/Models/HoaDon.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BTL06.Models
+{
+    
+    public class HoaDon
+    {
+        [Key]
+        public string MaHoaDon {get; set; }
+        public string  {get; set; }
+        public string PhoneNumber {get; set; }
+        public string Address {get; set; }
+           
+    }
+    
+}
